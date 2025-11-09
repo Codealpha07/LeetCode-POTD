b79f2c18736868ca67a8e7a48b919f7e00df4477
Devu Gupta <145492650+Codealpha07@users.noreply.github.com>
2025-11-09 10:55:41 +0530

    Time: 0 ms (100%), Space: 8 MB (6.11%) - LeetHub

---
+++ LeetCode/Easy/2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero-11-09-2025-10-55-36.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countOperations(int n1, int n2) {
+        int op = 0;
+        while(n1!=0 && n2!=0){
+            if(n1>=n2){
+                op+=n1/n2;
+                n1=n1%n2;
+            }
+            else{
+                op+=n2/n1;
+                n2=n2%n1;
+            }
+        }
+        return op;
+    }
+};
