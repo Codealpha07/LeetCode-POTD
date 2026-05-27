95016844e4e7b6b09738ea240ffb6e0de3f22f11
Devu Gupta <145492650+Codealpha07@users.noreply.github.com>
2026-05-27 14:03:42 +0530

    Time: 66 ms (77.25%), Space: 24.2 MB (80.34%) - LeetHub

---
+++ LeetCode/Medium/3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii-05-27-2026-14-03-41.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        vector<int> store(26, -1);
+        for(int i=0; i<word.length(); i++){
+            if(word[i]>='a'){
+                if(store[word[i]-'a']==-1) store[word[i]-'a']=0; 
+                if(store[word[i]-'a']==1) store[word[i]-'a']=-2;  
+            }
+            else {
+                if(store[word[i]-'A']==0) store[word[i]-'A']=1;
+                else if(store[word[i]-'A']==-1) store[word[i]-'A'] = -2;
+            }
+        }     
+        int count = 0;
+        for(int i=0; i<26; i++){
+            if(store[i]==1) count++;
+        }
+        return count;
+    }
+};
