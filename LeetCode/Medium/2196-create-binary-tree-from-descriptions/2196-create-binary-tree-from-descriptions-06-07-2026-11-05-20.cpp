c806bf7261102f17b8a69436ba865294c215ea31
Devu Gupta <145492650+Codealpha07@users.noreply.github.com>
2026-06-07 11:05:22 +0530

    Time: 183 ms (73.22%), Space: 278.9 MB (84.76%) - LeetHub

---
+++ LeetCode/Medium/2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions-06-07-2026-11-05-20.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> m;
+        unordered_set<int> children;
+        for(auto &d : descriptions) {
+            int p = d[0], c = d[1], isLeft = d[2];
+            if(!m.count(p)) m[p] = new TreeNode(p);
+            if(!m.count(c)) m[c] = new TreeNode(c);
+
+            if(isLeft) m[p]->left = m[c];
+            else m[p]->right = m[c];
+            children.insert(c);
+        }
+        for(auto &d : descriptions) {
+            int p = d[0];
+            if(!children.count(p)) return m[p];
+        }
+        return nullptr;
+    }
+};
