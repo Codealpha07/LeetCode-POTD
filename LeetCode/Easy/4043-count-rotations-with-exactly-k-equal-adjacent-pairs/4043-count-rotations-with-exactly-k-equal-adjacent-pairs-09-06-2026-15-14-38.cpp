d6b4100c017a5c97ddd58a6e4d040c27f706dca8
Devu Gupta <145492650+Codealpha07@users.noreply.github.com>
2026-09-06 15:14:39 +0530

    Time: 0 ms (100%), Space: 9.4 MB (76.36%) - LeetHub

---
+++ LeetCode/Easy/4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs-09-06-2026-15-14-38.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countRotations(string s, int k) {
+        int n = s.length();
+        int total = 0;
+        for(int i=0; i<n; i++){
+            if(s[i] == s[(i+1+n)%n]) total++;
+        }
+        if(total==k) return n-total;
+        if(k==total-1) return total;
+        return 0;
+    }
+};
